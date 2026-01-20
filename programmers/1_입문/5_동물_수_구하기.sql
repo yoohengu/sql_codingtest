68d872a36674bb0e5a1cbb246a3504a715a4dacd
yoohengu <yoohgxxx@gmail.com>
2026-01-20 22:59:33 +0900

    [프로그래머스] 입문 - 동물 수 구하기 (#5)

---
+++ programmers/1_입문/5_동물_수_구하기.sql
@@ -0,0 +1,8 @@
+-- 동물 수 구하기
+-- 프로그래머스 입문 (⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59406
+-- 작성자: 유현규
+-- 작성일: 2026. 01. 20. 22:59:33
+
+select count(*) as count
+from ANIMAL_INS;
