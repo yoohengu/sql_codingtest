494f3cdc5104bc6100abdc213e7525935e866096
yoohengu <yoohgxxx@gmail.com>
2026-01-20 23:00:19 +0900

    [프로그래머스] 입문 - 최솟값 구하기 (#9)

---
+++ programmers/1_입문/9_최솟값_구하기.sql
@@ -0,0 +1,10 @@
+-- 최솟값 구하기
+-- 프로그래머스 입문 (⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59038
+-- 작성자: 유현규
+-- 작성일: 2026. 01. 20. 23:00:18
+
+-- 코드를 입력하세요
+SELECT DATETIME AS 시간
+FROM ANIMAL_INS
+ORDER BY DATETIME LIMIT 1
