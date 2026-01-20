40bf005c70fa2a46753ee03c7a8ff9efbbff3fa9
yoohengu <yoohgxxx@gmail.com>
2026-01-20 23:00:05 +0900

    [프로그래머스] 입문 - 상위 n개 레코드 (#8)

---
+++ programmers/1_입문/8_상위_n개_레코드.sql
@@ -0,0 +1,10 @@
+-- 상위 n개 레코드
+-- 프로그래머스 입문 (⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59405
+-- 작성자: 유현규
+-- 작성일: 2026. 01. 20. 23:00:04
+
+-- 코드를 입력하세요
+SELECT NAME
+FROM ANIMAL_INS
+ORDER BY DATETIME LIMIT 1
