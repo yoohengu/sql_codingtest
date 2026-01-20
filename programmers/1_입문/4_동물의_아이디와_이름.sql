378b82589af04a01931b1d28dfbad8027226aca9
yoohengu <yoohgxxx@gmail.com>
2026-01-20 22:59:20 +0900

    [프로그래머스] 입문 - 동물의 아이디와 이름 (#4)

---
+++ programmers/1_입문/4_동물의_아이디와_이름.sql
@@ -0,0 +1,9 @@
+-- 동물의 아이디와 이름
+-- 프로그래머스 입문 (⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59403
+-- 작성자: 유현규
+-- 작성일: 2026. 01. 20. 22:59:20
+
+select ANIMAL_ID, NAME
+from ANIMAL_INS
+order by ANIMAL_ID asc;
